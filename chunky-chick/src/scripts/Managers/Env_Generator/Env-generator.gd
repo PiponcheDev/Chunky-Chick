15fd1457b9d21b38515ebff438ccf15590c91650
Your Name <you@example.com>
2026-03-30 23:33:00 +0300

    Fixed majority of bugs

--- chunky-chick/src/scripts/Managers/Env_Generator/Env-generator.gd
+++ chunky-chick/src/scripts/Managers/Env_Generator/Env-generator.gd
@@ -1,125 +1,97 @@
 extends Node2D
 
-func _ready():
-	_setup_spawnzones()
-	_generate_map()
-
-
-@export var Mapsize :Vector2 = Vector2(100,100)
-@export var OriginOffset : Vector2 = Vector2(0,0)
-#Describes how the map shall be generated around the origin of the node.
-@export var UseSpawnZones : bool = true
-
-@export_subgroup("Frequencies")
-@export var itemFrequency = 10
-@export var talismanFrequency = 0
-@export var foodFrequency = 10
-@export var trashcanFrequency = 10
-@export var dumpsterFrequency = 10
-#Per 100 pixels on a line, in theory...
-var FrequencyRandomness = 3
+@export var Mapsize : Vector2 = Vector2(2000, 2000) 
 
 @export_subgroup("Lists")
-@export var items : Array[PackedScene] = [preload("uid://e3t6v4jtmdh6")]
-@export var talismen : Array[SpringBoneCollisionCapsule3D] 
-@export var foods : Array[PackedScene] = [preload("uid://vxnhxnc8fji4")] 
-@export var trashCans : Array[SpringBoneCollisionCapsule3D] 
-@export var dumpsters : Array[SpringBoneCollisionCapsule3D] 
-#Obviously needs to be changed to the correct data structure
+@export var items : Array[PackedScene] = [preload("res://src/tscn/items.tscn")]
+@export var foods : Array[PackedScene] = [preload("res://src/tscn/Food.tscn")] 
+@export var trashCans : Array[PackedScene] = [preload("res://src/tscn/Structures/trash_can.tscn")] 
+@export var talismen : Array[PackedScene] = [] 
+@export var dumpsters : Array[PackedScene] = [] 
 
-@export_subgroup("Clumps")
-var currentClumpPos:Vector2 = Vector2(0,0)
-var currentClumpWeight:int = 0
-#How many spawns are currently in the clump
-@export var clumpCapacity = 5
-#How many spawns fits in a clump
-@export var clumpDensity = 5
-#How close to the center the spawns are
-#There is a possibility of the items spilling out of the spawn zone if this is high enough
-var amountOfClumps = 0
+@export_subgroup("Clump Settings")
+@export var defaultClumpCapacity = 4
+@export var clumpDensity = 20 
 
+# Internals for clumping logic
+var currentClumpPos : Vector2 = Vector2(0, 0)
+var currentClumpWeight : int = 0
 
-var item_spawn_zones :Array[CollisionShape2D]= []
-var talimen_spawn_zones :Array[CollisionShape2D]= []
-var food_spawn_zones :Array[CollisionShape2D]= []
-var trashcan_spawn_zones :Array[CollisionShape2D]= []
-var dumpster_spawn_zones :Array[CollisionShape2D]= []
+var item_spawn_zones : Array[CollisionShape2D] = []
+var talimen_spawn_zones : Array[CollisionShape2D] = []
+var food_spawn_zones : Array[CollisionShape2D] = []
+var trashcan_spawn_zones : Array[CollisionShape2D] = []
+var dumpster_spawn_zones : Array[CollisionShape2D] = []
+
+func _ready():
+	await get_tree().process_frame 
+	_setup_spawnzones()
+	_generate_map()
 
 func _setup_spawnzones():
 	for child in self.get_children():
 		if child is Area2D:
-			print(child)
-			if child.items:
-				item_spawn_zones.append(child.get_child(0))
-				print(child.get_child(0))
-			if child.foods:
-				food_spawn_zones.append(child.get_child(0))
-			print(child.get_child(0))
+			var shape_node = child.get_child(0)
+			if not shape_node is CollisionShape2D: continue
 
-			if child.talimen:
-				talimen_spawn_zones.append(child.get_child(0))
-			if child.trashcans:
-				trashcan_spawn_zones.append(child.get_child(0))
-			if child.dumpsters:
-				dumpster_spawn_zones.append(child.get_child(0))
-	print(item_spawn_zones)
-	print(food_spawn_zones)
-
+			if child.get("items") == true: item_spawn_zones.append(shape_node)
+			if child.get("foods") == true: food_spawn_zones.append(shape_node)
+			if child.get("talismen") == true: talimen_spawn_zones.append(shape_node)
+			if child.get("trashcans") == true: trashcan_spawn_zones.append(shape_node)
+			if child.get("dumpsters") == true: dumpster_spawn_zones.append(shape_node)
 
 func _generate_map():
-	spawn_object(items, item_spawn_zones,itemFrequency)
-	#spawn_object(talimen_spawn_zones,talismanFrequency)
-	spawn_object(foods, food_spawn_zones,foodFrequency)
+	# 1. Trash Cans: Exactly 5 total, Clump size of 1
+	_spawn_logic(trashCans, trashcan_spawn_zones, 5, 1, "Trash Cans")
+	
+	# 2. Food: Exactly 60 total, Uses default clump size
+	_spawn_logic(foods, food_spawn_zones, 60, defaultClumpCapacity, "Food")
+	
+	# 3. Items: Random between 10 and 20, Uses default clump size
+	var item_count = randi_range(10, 20)
+	_spawn_logic(items, item_spawn_zones, item_count, defaultClumpCapacity, "Items")
+	
+	# 4. Others (Optional)
+	_spawn_logic(talismen, talimen_spawn_zones, 2, 1, "Talismans")
+	_spawn_logic(dumpsters, dumpster_spawn_zones, 2, 1, "Dumpsters")
 
-
-func spawn_object(objects, spawn_zones, Frequency):
-	var zone = spawn_zones.pick_random()
-	currentClumpPos = _get_randomSpotInZone(zone.shape.size,false) + zone.position + zone.get_parent().position - Vector2(zone.shape.size.x/2,zone.shape.size.y/2)
-	if len(spawn_zones) == 0:
+func _spawn_logic(objects: Array[PackedScene], zones: Array[CollisionShape2D], count: int, clump_limit: int, debug_name: String):
+	if objects.is_empty() or zones.is_empty():
 		return
-	var i = 0
-	while i < _get_AmountOFSpawns(Frequency):
-		#print("Items Spawned: "+str(i))
-		var spawnPos = getSpawnposInClump(spawn_zones)
-		
-		var newitem = objects.pick_random().instantiate()
-		newitem.global_position = spawnPos
-		self.add_child(newitem)
-		
-		print("spawnpos:" + str(spawnPos))
-	#	print(currentClumpPos)
-	#	print(currentClumpWeight)
-		i += 1
+	
+	print("Spawning ", count, " ", debug_name)
+	
+	# Reset clumping for the new category
+	currentClumpWeight = 0 
+	
+	for i in range(count):
+		var spawnPos = getSpawnposInClump(zones, clump_limit)
+		var new_obj = objects.pick_random().instantiate()
+		add_child(new_obj)
+		new_obj.global_position = spawnPos
 
-func getSpawnposInClump(Spawn_Zones:Array[CollisionShape2D]):
-	if randi_range(0,clumpCapacity-currentClumpWeight) > 0:
+func getSpawnposInClump(Spawn_Zones: Array[CollisionShape2D], clump_limit: int):
+	# If we are below the specific clump limit for this item type, stay near center
+	if currentClumpWeight < clump_limit and currentClumpWeight > 0:
 		currentClumpWeight += 1
-		return currentClumpPos + _get_randomSpotInZone(Vector2(clumpDensity,clumpDensity))
+		return currentClumpPos + _get_random_offset(clumpDensity)
 	else:
-		#currentClumpPos = _get_randomSpotInZone(Mapsize,false)
-		var zone:CollisionShape2D = Spawn_Zones.pick_random()
-		currentClumpPos = _get_randomSpotInZone(zone.shape.size,false) + zone.position + zone.get_parent().position - Vector2(zone.shape.size.x/2,zone.shape.size.y/2)
-		#The reson for the last part of the previous line is unknown, but it did fix an issue
+		# Start a brand new clump (or a single item)
+		var zone = Spawn_Zones.pick_random()
+		var size = _get_shape_size(zone)
+		
+		currentClumpPos = zone.global_position + Vector2(
+			randf_range(-size.x/2, size.x/2),
+			randf_range(-size.y/2, size.y/2)
+		)
+		
 		currentClumpWeight = 1
-		amountOfClumps += 1
-		return currentClumpPos + _get_randomSpotInZone(Vector2(clumpDensity,clumpDensity))
+		return currentClumpPos
 
+func _get_shape_size(zone: CollisionShape2D) -> Vector2:
+	if zone.shape is RectangleShape2D: return zone.shape.size
+	if zone.shape is CircleShape2D: return Vector2(zone.shape.radius * 2, zone.shape.radius * 2)
+	return Vector2(50, 50)
 
-func _get_randomSpotInZone(area:Vector2, centered:bool = true):
-	if centered:
-		#return Vector2(0,0)
-		return Vector2(randi_range(-area.x,area.x),randi_range(-area.y,area.y))
-	else:
-		print(area.x)
-		print(area.y)
-		var returnvalue = Vector2(randi_range(0,area.x),randi_range(0,area.y))
-		print("return: " + str(returnvalue))
-		
-		return returnvalue
-
-func _get_AmountOFSpawns(Frequency):
-	var amountOfSpawns = (Mapsize.x+Mapsize.y)/(Frequency + randi_range(-FrequencyRandomness,FrequencyRandomness))
-	return amountOfSpawns
-
-		
-	return false
+func _get_random_offset(dist: float) -> Vector2:
+	return Vector2(randf_range(-dist, dist), randf_range(-dist, dist))
